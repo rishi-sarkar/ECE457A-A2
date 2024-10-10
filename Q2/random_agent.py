f992ce920894f1f44d18817f78dbe2c2b5503eab
Rishi Sarkar <rishisarkar273@gmail.com>
2024-10-10 04:52:26 -0400

    working smart agent

--- Q2/random_agent.py
+++ Q2/random_agent.py
@@ -4,18 +4,21 @@ import random
 
 
 class RandomAgent(Agent):
-    def randomMove(self):
-        if (self.lost()):
+    def move(self, state):
+        if (self.lost(state)):
             return False
         # assign square a random occupied square that has >0 open paths
-        square = random.choice(self.occupiedSquares)
+        occupiedSquares = self.occupiedSquares(state)
+        square = random.choice(occupiedSquares)
         
-        while (len(square.neighbors) == 0):
-            square = random.choice(self.occupiedSquares)
+        while (len(square.neighbors(state)) == 0):
+            square = random.choice(occupiedSquares)
         
-        direction = random.choice(square.neighbors)
+        direction = random.choice(square.neighbors(state))
         
-        self.place(square, direction)
+        self.place(square, direction, state)
+        
+        return True
         
         
             
